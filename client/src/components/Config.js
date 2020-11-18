771b2d0eb52998cddc3e9b1b1bc74d2ec6297131
James Jeffeson <jrjefferson830@gmail.com>
2020-11-17 23:22:10 -0600

    Settting up DB and  Movie request function.

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'c92a400d44a643ec644a7b12eb549534';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
