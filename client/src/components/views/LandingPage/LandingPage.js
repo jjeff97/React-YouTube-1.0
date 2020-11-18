86b64607b2ff2796ee67b262873455ca8a27bf84
James Jeffeson <jrjefferson830@gmail.com>
2020-11-17 23:37:33 -0600

    movie request function setup

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+
+import React, {useEffect} from 'react';
+import { FaCode } from 'react-icons/fa';
+import { API_URL, API_KEY } from '../../Config';
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="app">
+        <FaCode style={{ fontSize: '4rem' }} />
+        <br />
+        <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+      </div>
+      <div style={{ float: 'right' }}>
+        Thanks For Using This Boiler Plate by John Ahn
+      </div>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
