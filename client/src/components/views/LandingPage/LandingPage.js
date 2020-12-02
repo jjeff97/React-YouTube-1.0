abfb25e7318597ca54393e62da4649b14bc734b9
James Jeffeson <jrjefferson830@gmail.com>
2020-12-01 23:38:51 -0600

    Main image rendering issues

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -43,14 +43,14 @@ fetchMovies(endpoint);
         style={{
           background: `linear-gradient(to bottom, rgba(0,0,0,0)
 39%,rgba(0,0,0,0)
-41%rgba(0,0,0,0.65)
+41%,rgba(0,0,0,0.65)
 100%),
 url(''), #1c1c1c`,
           height: '500px',
           backgroundSize: '100%, cover',
           backgroundPosition: 'center, center',
           width: '100%',
-          position: 'relative',
+          position: 'relative'
         }}
       ></div>
       <div>
