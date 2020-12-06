c3b95e9f21235fe88eb6e1b57684d7f44e7a9906
James Jeffeson <jrjefferson830@gmail.com>
2020-12-06 15:33:38 -0600

    Movie details page build

--- client/src/components/views/MovieDetailsPage/MovieDetailsPage.js
+++ client/src/components/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { API_URL, API_KEY, IMAGE_URL } from '../../Config';
 
 function MovieDetailsPage() {
+
+    useEffect(() => {
+       fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+        
+    }, [])
+
     return (
         <div>
             
